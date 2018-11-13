21a4789b7f0f078c7a75dd8a3a3492adec0fb501
Jason Chang <jasonchang0@berkeley.edu>
2018-11-12 17:41:41 -0800

    text display

--- image_display.py
+++ image_display.py
@@ -1,9 +1,10 @@
 import pygame
+import time
 
 pygame.init()
 
-display_width = 863
-display_height = 576
+display_width = 1232
+display_height = 816
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -13,7 +14,13 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Display Sample Image')
 clock = pygame.time.Clock()
 
-img = pygame.image.load('sample.jpg')
+img = pygame.image.load('sample_reduce.jpg')
+img = pygame.transform.scale(img, (display_width // 4, display_height // 4))
+img = pygame.transform.rotate(img, 90)
+
+img_width = display_height // 4
+img_height = display_width // 4
+
 
 def dislpay_img(toBeDisplayed, x, y):
     '''
@@ -29,24 +36,87 @@ def dislpay_img(toBeDisplayed, x, y):
     '''
     gameDisplay.blit(toBeDisplayed, (x, y))
 
-crashed = False
 
-while not crashed:
-    # Event handling loop
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
+def text_objects(text, font):
+    text_surf = font.render(text, True, black)
+    return text_surf, text_surf.get_rect()
 
-        elif event.type == pygame.QUIT:
-            crashed = True
 
-        # print(event)
+def message_display(text):
+    text_style = pygame.font.Font('freesansbold.ttf', 115)
+    text_surf, text_rect = text_objects(text, text_style)
+    text_rect.center = ((display_width/2), (display_height/2))
 
-    dislpay_img(img, 0, 0)
-
-    # Updating the current display at 60fps
+    gameDisplay.blit(text_surf, text_rect)
     pygame.display.update()
-    clock.tick(60)
+
+    time.sleep(10)
+
+    game_loop()
+
+
+def crash():
+    message_display('You Crashed')
+
+
+def game_loop():
+    x = display_width * 0.45
+    y = display_height * 0.60
+
+    dx = 0
+
+    exit_game = False
+
+    while not exit_game:
+        # Event handling loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # exit_game = True
+                pygame.quit()
+                quit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    dx = -10
+
+                elif event.key == pygame.K_RIGHT:
+                    dx = 10
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    dx = 0
+
+            # print(event)
+
+        x += dx
+
+        # Changing background color
+        gameDisplay.fill(white)
+        dislpay_img(img, x, y)
+
+        if not 0 < x < display_width - img_width:
+            crash()
+
+        # elif not 0 < y < display_height - img_height:
+        #     exit_game = True
+
+        # Updating the current display at 60fps
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 
 pygame.quit()
 quit()
+
+
+
+
+
+
+
+
